19845a83022333ad13a7a13581b688945dc60739
ahnafhasnainAiub <ahnafhasnain7860@gmail.com>
2024-09-14 13:06:46 +0600

    redux toolkit and useMemo()

---
+++ useMemo() and useCallback()/useMemo()/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Memo from './components/Memo'
+
+function App() {
+
+  return (
+    <div>
+      
+      <h1>Ahnaf Hasnain the Boss</h1>
+
+      <Memo></Memo>
+     
+    </div>
+  )
+}
+
+export default App
---
+++ useMemo() and useCallback()/useMemo()/src/components/Memo.jsx
@@ -0,0 +1,59 @@
+import React, { useMemo, useState } from 'react'
+
+
+function Memo() {
+    
+    const [counter1, setCounter1]= useState(0);
+    const [counter2, setCounter2]= useState(0);
+
+  const increment1 = ( )=> {
+      setCounter1(counter1 + 1);
+  }
+
+  const increment2 = ( )=> {
+    setCounter2(counter2 + 1);
+  }
+
+
+  //Checking purpose
+//    function Even() {
+    
+//     console.log('This event take time');
+//      let i = 0;
+//      while (i <= 1000000000) i++; 
+
+//     return counter1 % 2 === 0;
+
+//   }
+
+    const EvenOdd = useMemo(()=> {
+        console.log('This event take time');
+        let i = 0;
+        while(i <= 1000000000) i++; 
+   
+       return counter1 % 2 === 0;
+    }, [counter1])
+    
+
+    return (
+      <div>
+
+        <div>
+          <h1>Button - 1</h1>
+          <button onClick={increment1}>Click Here</button>
+          <br />
+          <span>{counter1}</span>
+          <br />
+          <span>{EvenOdd ? "This is Even Number" : "This is Odd Number"}</span>
+        </div>
+
+        <div>
+          <h1>Button - 2</h1>
+          <button onClick={increment2}>Click Me</button>
+          <div>{counter2}</div>
+        </div>
+      </div>
+    );
+}
+
+export default Memo;
